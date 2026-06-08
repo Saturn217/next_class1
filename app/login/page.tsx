257ffcd2f74adb323531945a93366e7fa6655d58
0xSaturn <mubarakoladele98@gmail.com>
2026-06-08 14:54:36 +0100

    Remove unused CSS variables and add login page with email and password fields

---
+++ app/login/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import page from "../page";
+
+const Page = () => {
+  return (
+    <>
+      <div className="flex h-screen justify-center items-center px-2">
+        
+        <div className= "bg-gray-50 shadow-lg rounded-sm p-10 w-full md:w-1/2  xl:w-1/3 flex-col gap-2 flex justify-center /items-center">
+          <h1 className= "text-center text-2xl font-bold">Login Here</h1>
+          <div>
+            <label htmlFor="Email">Email:</label>
+            <input type="text" className="border outline-1 focus:outline-green-300 p-2 w-full rounded-sm" autoFocus />
+          </div>
+
+          <div>
+            <label htmlFor="Password">Password:</label>
+            <input type="password" className="border outline-1 focus:outline-green-300 p-2 w-full rounded-sm" />
+          </div>
+
+            <button className=" py-2 bg-black text-white rounded-sm hover:bg-gray-800 hover:cursor-pointer hover:translate-y-1 hover:transition">Login</button>
+        </div>
+
+      </div>
+    </>
+  );
+};
+
+export default Page;
